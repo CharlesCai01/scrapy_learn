263832aad46ca7e8de0228d273227ccaf78670cb
RainOfPhone <792209198@qq.com>
2018-05-05 21:21:29 +0800

    修改了显示为Json数组的问题

--- scrapy_learn/spiders/tutorSpider.py
+++ scrapy_learn/spiders/tutorSpider.py
@@ -10,14 +10,15 @@ class TutorSpider(scrapy.Spider):
     start_urls=['http://ggglxy.scu.edu.cn/index.php?c=article&a=type&tid=18']
     
     def parse(self,response):
-        for href in response.xpath("//div[@class='l fl']/a/@href"):
-            url = response.urljoin(href.extract())
+        for tutor in response.xpath("//div[@class='l fl']"):
+            href = tutor.xpath("a/@href").extract_first()
+            url = response.urljoin(href)
             ## 将得到的页面地址传送给单个页面处理函数进行处理 -> parse_details()
             yield scrapy.Request(url,callback=self.parse_details)
 
-            ## 是否还有下一页，如果有的话，则继续
-        next_page=response.xpath("//div[@class='pager cf tc pt10 pb10 mobile_dn']/li[last()-1]/a/@href").extract_first()
 
+        ## 是否还有下一页，如果有的话，则继续
+        next_page=response.xpath("//div[@class='pager cf tc pt10 pb10 mobile_dn']/li[last()-1]/a/@href").extract_first()
         if next_page is not None:
             next_pages = response.urljoin(next_page)
             ## 将 「下一页」的链接传递给自身，并重新分析
@@ -29,7 +30,7 @@ class TutorSpider(scrapy.Spider):
         # 初始化item对象保存爬取的信息
         item = TutorItem()
         # 这部分是爬取部分，使用xpath的方式选择信息，具体方法根据网页结构而定
-        item['name'] = response.xpath("//div[@class='r fr']/h3/text()").extract()
-        item['position'] = response.xpath("//div[@class='r fr']/p/text()").extract()
-        item['description'] = response.xpath("//div[@class='r fr']/div/text()").extract()
+        item['name'] = response.xpath("//div[@class='r fr']/h3/text()").extract_first()
+        item['position'] = response.xpath("//div[@class='r fr']/p/text()").extract_first()
+        item['description'] = response.xpath("//div[@class='r fr']/div/text()").extract_first()
         yield item
